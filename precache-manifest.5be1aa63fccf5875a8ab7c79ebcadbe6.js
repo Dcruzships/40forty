1a523239f45b5b87aebde6f2e3b73bf803f43359
dcruzships <bvd5889@rit.edu>
2020-05-10 22:57:53 -0500

    Updates

---
+++ precache-manifest.5be1aa63fccf5875a8ab7c79ebcadbe6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01515ab775297090f787d0cef749f8df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4521b9ccabab837234c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "45629aaf8d1eaac2fb42",
+    "url": "/static/js/2.12ca80fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.12ca80fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4521b9ccabab837234c",
+    "url": "/static/js/main.f7b86a3c.chunk.js"
+  },
+  {
+    "revision": "3d2e91cbd05ccbe18498",
+    "url": "/static/js/runtime-main.9b1eafaa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
