dcc2b28f620c4ab550a8da8ff3fe1da21535f224
dcruzships <bvd5889@rit.edu>
2020-05-11 20:35:29 -0500

    React Countdown

--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Countdown from 'react-countdown';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Countdown date={'June 20, 2020 00:00:00'} />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
